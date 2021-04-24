10bc4721dd7e2ca3ceb56cceed4bdd3e24059cad
quxiangjia <quxiangjia@yeah.net>
2021-04-24 11:24:13 +0800

    测试接受文件和字段代码

--- src/main/java/com/mieba/demo/controller/CostController.java
+++ src/main/java/com/mieba/demo/controller/CostController.java
@@ -10,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,9 +35,12 @@ public class CostController {
     @Autowired
     CostService costService;
 
-    @RequestMapping(value = "/find", method = RequestMethod.GET)
+    MultipartHttpServletRequest multipartHttpServletRequest;
+
+    @RequestMapping(value = "/find", method = RequestMethod.POST)
     @ResponseBody
-    public List<Cost> findAll(@RequestParam(value = "id", defaultValue = "0") int id) throws IOException {
+    public List<Cost> findAll(@RequestParam(value = "id", defaultValue = "0") int id,MultipartHttpServletRequest multipartHttpServletRequest) throws IOException {
+        this.multipartHttpServletRequest=multipartHttpServletRequest;
         return doFind(id);
     }
 
@@ -58,6 +67,25 @@ public class CostController {
     }
 
     private List<Cost> doFind(int id) {
+        //接收参数
+        String testData=request.getParameter("testData");
+        System.out.println(testData);
+        MultipartFile model = multipartHttpServletRequest.getFile("modelFile");
+
+        //转换文件
+        StringBuilder stringBuilder = new StringBuilder();
+        try{
+            InputStream inputStream = model.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.defaultCharset()));
+            String line = "";
+            while((line= bufferedReader.readLine())!=null){
+                stringBuilder.append(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        String content = stringBuilder.toString();
+        System.out.println(content);
         List<Cost> costList=new ArrayList<>();
         if (id > 0) {
             costList.add(costService.queryById(id));
